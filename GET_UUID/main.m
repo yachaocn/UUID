0d150d0eeaed7c8fc9045f666b9f10c4bca2e8b3
yachaocn <zyachao@163.com>
2015-12-25 14:15:21 +0800

    Initial Commit

---
+++ GET_UUID/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GET_UUID
+//
+//  Created by yachaocn on 15/12/25.
+//  Copyright © 2015年 NavchinaMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ GET_UUIDTests/GET_UUIDTests.m
@@ -0,0 +1,39 @@
+//
+//  GET_UUIDTests.m
+//  GET_UUIDTests
+//
+//  Created by yachaocn on 15/12/25.
+//  Copyright © 2015年 NavchinaMacBook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GET_UUIDTests : XCTestCase
+
+@end
+
+@implementation GET_UUIDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
